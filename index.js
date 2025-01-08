29ee3a89a104ffba94ebdd958adb8f29159a8596
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-08 15:36:36 -0800

    updated files

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Jason Yale');
-});
-
-app.get('/contacts', (req, res) => {
-  fs.readFile('contacts.json', 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).send('Error reading contacts data');
-    }
-
-    const contacts = JSON.parse(data);
-    res.json(contacts);
-  });
-});
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+    console.log(`Running on port ${port}`)
+})

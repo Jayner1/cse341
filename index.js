c9a4eae57ac8fd9963a8a5d16c3c788592926d5c
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-08 15:10:24 -0800

    get contacts

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Jason Yale');
 });
 
+app.get('/contacts', (req, res) => {
+    res.json({
+      contacts: [
+        { name: 'Jason Yale', email: 'jasontyale@gmail.com' }
+      ]
+    });
+  });
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });

a97c4125077e59e10be5cb60e84d8763888c2e5d
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-08 15:14:14 -0800

    updated contacts and index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 app.get('/contacts', (req, res) => {
     res.json({
       contacts: [
-        { name: 'Jason Yale', email: 'jasontyale@gmail.com' }
+        { name: '', email: '', phone: "" }
       ]
     });
   });

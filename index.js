7635c16ebf2f9b75a502343098c0806b5e412487
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-18 18:18:28 -0800

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 

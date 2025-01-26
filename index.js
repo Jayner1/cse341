d3ed2843e5be6ef3d95cdb52f98d7b4672add130
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-25 18:14:24 -0800

    added json

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));

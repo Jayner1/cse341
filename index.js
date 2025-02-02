11686fb468dd70d2ba8aa44b7f9cc1c4b7e07ccd
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-02-01 16:21:08 -0800

    fixes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');  // Import cors package
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Enable CORS for all routes
+app.use(cors());
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
+    // You can set specific headers here if you want more control
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })

a7d363778ed94ab625b3d27bc2bd26803a5144c1
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-02-08 17:42:10 -0800

    task updated

--- Task/index.js
+++ Task/index.js
@@ -1,28 +1,23 @@
 require('dotenv').config();
-console.log(process.env.MONGO_URI);  // Log the Mongo URI to check if it's being loaded correctly
+console.log(process.env.MONGO_URI);
 
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 
-// Add Swagger UI dependencies
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
 
-// Routes
 const taskRoutes = require('./routes/taskRoutes');
 const Task = require('./models/taskModel');
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
-// Swagger UI route for documentation
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
